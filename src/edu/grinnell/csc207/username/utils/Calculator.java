04b54279713df71d2972dada33ebc00c00c18a94
hardtmad <hardtmad@grinnell.edu>
2014-02-05 22:44:01 -0600

    Add updates for all parts of HW 3. So sorry it's late, Sam!

--- src/edu/grinnell/csc207/username/utils/Calculator.java
+++ src/edu/grinnell/csc207/username/utils/Calculator.java
@@ -1,6 +1,64 @@
 package edu.grinnell.csc207.username.utils;
 
+import java.math.BigInteger;
+import java.lang.String;
+
 public class Calculator
 {
+  
+  /*Part E*/
+  public static BigInteger
+    eval0 (String str)
+  {
+    int strLength = str.length ();
+    char ch;
+    BigInteger eval = BigInteger.valueOf (0);
+    eval = BigInteger.valueOf(str.charAt(0) - '0');
+    
+    int number = 0;
+    char operation = 0;
 
+    for (int i = 2; i < strLength; i += 2)
+      {
+        ch = str.charAt (i);
+
+        if ("+-*/^".indexOf (ch) != -1)
+          {
+            operation = ch;
+
+          } // if
+
+        else
+          {
+            number = (int) ch - '0';
+
+            switch (operation)
+              {
+                case '+':
+                  eval = eval.add (BigInteger.valueOf (number));
+                  break;
+                case '-':
+                  eval = eval.subtract (BigInteger.valueOf (number));
+                  break;
+                case '*':
+                  eval = eval.multiply (BigInteger.valueOf (number));
+                  break;
+                case '/':
+                  eval = eval.divide (BigInteger.valueOf (number));
+                  break;
+                case '^':
+                  eval = eval.pow ((int) number);
+                  break;
+                default:
+                  break;
+              } // switch
+            
+            System.out.println ("i " + i + " eval " + eval);
+          } // else
+
+       
+      }
+
+    return eval;
+  } // eval0 (String)
 }
--- src/edu/grinnell/csc207/username/utils/StringUtils.java
+++ src/edu/grinnell/csc207/username/utils/StringUtils.java
@@ -2,6 +2,7 @@ package edu.grinnell.csc207.username.utils;
 
 import java.lang.String;
 import java.util.Arrays;
+import java.io.PrintWriter;
 
 public class StringUtils
 {
@@ -18,16 +19,107 @@ public class StringUtils
         if (newIndex == -1)
           {
             newIndex = str.length ();
-          } //if
+          } // if (newIndex == -1)
         strArray[strArrayIndex] = str.substring (fromIndex, newIndex);
         strArrayIndex++;
         fromIndex = newIndex + 1;
-      } //while
-    String[] strArrayTruncated = new String[strArrayIndex];
-    strArrayTruncated = Arrays.copyOf (strArray, strArrayIndex);
-    return strArrayTruncated;
-  }// splitAt (String, char)
+      } // while (fromIndex < str.length ())
 
+    String[] strArrayTruncated = new String[strArrayIndex];
+
+    if (newIndex == str.length () - 1)
+      {
+        strArrayTruncated = Arrays.copyOf (strArray, strArrayIndex + 1);
+        strArrayTruncated[strArrayIndex] = "";
+      } // if (newIndex == str.length() - 1)
+    else
+      {
+        strArrayTruncated = Arrays.copyOf (strArray, strArrayIndex);
+      } // else
+
+    return strArrayTruncated;
+  } // splitAt (String, char)
+
+  public static void
+    printValues (PrintWriter pen, String[] array)
+
+  {
+    int length = array.length;
+    if (length == 0)
+      {
+        pen.println ("{}");
+      }
+    else
+      {
+        pen.print ("{\"");
+        for (int i = 0; i < length - 1; i++)
+          {
+            pen.print ("\"" + array[i] + "\", ");
+          }
+        pen.println (array[length - 1] + "\"}");
+      }
+  }// printValues
+
+  public static String[]
+    splitCSV (String str)
+  {
+    PrintWriter pen = new PrintWriter (System.out, true);
+
+    /* length of str */
+    int strLength = str.length ();
+
+    /* array of substrings */
+    String[] output = new String[str.length ()];
+    int strArrayIndex = 0;
+
+    /* variables for traversing str */
+    char ch;
+    int index = 0;
+    int prevStrIndex = 0;
+    boolean quotesOpen = false;
+
+    for (; index < strLength; index++)
+      {
+        ch = str.charAt (index);
+
+        if (ch == ',' && quotesOpen == false)
+          {
+            output[strArrayIndex] = str.substring (prevStrIndex, index);
+            strArrayIndex++;
+            quotesOpen = false;
+            prevStrIndex = index + 1;
+
+          } // if (ch == ',')
+
+        if (ch == '"')
+          {
+            quotesOpen = !quotesOpen;
+
+            if (str.charAt (index + 1) == '"')
+              {
+                quotesOpen = !quotesOpen;
+              } // if(str.charAt(i+1) == '"')
+
+          } // if (ch == '"')
+
+      } // for (int i = 0; i < str.length (); i++)
+
+    if (prevStrIndex != index)
+      {
+        output[strArrayIndex] = str.substring (prevStrIndex, index);
+      }
+
+    String[] strArrayTruncated = new String[strArrayIndex];
+    strArrayTruncated = Arrays.copyOf (output, strArrayIndex + 1);
+
+    printValues (pen, output);
+    printValues (pen, strArrayTruncated);
+    return strArrayTruncated;
+
+  } // splitCSV (String)
+
+
+ 
   public static String
     deLeet (String str)
   {
@@ -68,18 +160,63 @@ public class StringUtils
             default:
               a--;
               break;
-          } //switch
+          } // switch (strTemp)
         a++;
         b++;
-      } //for
+      } // for (int i = 0; i < str.length (); i++)
     return newStr;
-  }//deLeet (String)
+  }// deLeet (String)
+
+  /*
+   * I found the code for this method from an isVowel method on Stack Overflow
+   * http
+   * ://stackoverflow.com/questions/19160921/how-do-i-check-if-a-char-is-a-vowel
+   */
+  public static int
+    countLeadingVowels (String str)
+  {
+    int counter = 0;
+    for (; counter < str.length (); counter++)
+      {
+        char ch = str.charAt (counter);
+        if ("AEIOUaeiou".indexOf (ch) != -1)
+          {
+            break;
+          } // if ("AEIOUaeiou".indexOf (ch) != -1)
+      } // for (; counter < str.length (); counter++)
+    return counter;
+  } // countLeadingVowels ()
+
+  public static String
+    nameGame (String name)
+  {
+    String verse = "";
+
+    int index = countLeadingVowels (name);
+    String shortName;
+    if (index == 0)
+      {
+        shortName = name.toLowerCase ();
+      } // if (index == 0)
+    else
+      {
+        shortName = name.substring (index);
+      } // else
+
+    verse += name + "! \n";
+    verse += name + ", " + name + " bo B" + shortName + " Bonana fanna"
+             + " fo F" + shortName + "\n";
+    verse += "Fee fy mo M" + shortName + ", " + name + "!\n";
+
+    return verse;
+  } // nameGame (String)
 
   public static void
     main (String[] args)
       throws Exception
   {
-    return;
-  }// main (String[])
 
-}// class StringUtils
+    return;
+  } // main (String[])
+
+} // class StringUtils
